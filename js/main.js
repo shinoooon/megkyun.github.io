19ca50aaab88d213e4303896ed18cd801aa105aa
shinoooon <n.shimbo.hs@gmail.com>
2021-07-04 21:06:32 +0900

    portforlio add

--- js/main.js
+++ js/main.js
@@ -15,3 +15,48 @@ $(function() {
       $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
       $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
   });
+
+  // portforilのfilter
+function portfoliofilter() {
+  $('.pill-button').click(function () {
+    var value = $(this).attr('data-filter');
+
+    if (value == "*") {
+      $('.portfolio-item').show('1000');
+      
+    } else {
+      $('.portfolio-item').not(value).hide('3000');
+      $('.portfolio-item').filter(value).show('3000');
+    }
+
+    // activeの切替え
+    $('.pill-button').removeClass('active');
+    $(this).addClass('active');
+
+  });
+ 
+}
+
+$(document).ready(function () {
+  "use strict";
+
+  // navColorIn();
+  portfoliofilter();
+  portfolioPopup();
+  // titleIn();
+  
+});
+
+function portfolioPopup() {
+  if (('.portfolio-items').length > 0) {
+      $('.portfolio-items').each(function() {
+          $(this).magnificPopup({
+              delegate: '.js-zoom-gallery',
+              type: 'image',
+              gallery: {
+                  enabled:true
+              }
+          });
+      });
+  }
+}
